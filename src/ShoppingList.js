c84ce3c82a3d69dfc9cb6d039b53813b595cf2d0
imallinson <imallinson1001@gmail.com>
2019-02-03 15:18:16 +0000

    neaten up code

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -26,14 +26,14 @@ class ShoppingList extends Component {
             url: "http://localhost:8080/shopping-list/rest/ingredient/get/" + sessionStorage.getItem("username"),
             responseType: 'json'
         })
-        .then(function (response) {
-            actuallyThis.setState({
-                shoppingList: response.data
+            .then(function (response) {
+                actuallyThis.setState({
+                    shoppingList: response.data
+                })
             })
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
+            .catch(function (error) {
+                console.log(error);
+            })
     }
 
     clearList = () => {
@@ -43,14 +43,14 @@ class ShoppingList extends Component {
             url: "http://localhost:8080/shopping-list/rest/account/clear/" + sessionStorage.getItem("username"),
             responseType: 'json'
         })
-        .then(function (response) {
-            actuallyThis.setState({
-                shoppingList: []
+            .then(function (response) {
+                actuallyThis.setState({
+                    shoppingList: []
+                })
             })
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
+            .catch(function (error) {
+                console.log(error);
+            })
     }
 
     handleUpdate = () => {
@@ -63,26 +63,26 @@ class ShoppingList extends Component {
     }
 
     render() {
-        const listComponents = this.state.shoppingList.map((i) => 
+        const listComponents = this.state.shoppingList.map((i) =>
             (<div className="row" id={i.ingredientID} >
                 <Ingredient ingredient={i} onUpdate={this.handleUpdate} />
             </div>));
 
-        return(
+        return (
             <div className="container" id="listPage" >
-                <br/>
+                <br />
                 <div className="row">
                     <div className="col s4 offset-s4 center">
                         <button className="btn grey darken-2" onClick={this.clearList} >Clear Shopping List</button>
                     </div>
-                    <div className="col s1 offset-s2 right">                        
+                    <div className="col s1 offset-s2 right">
                         <button className="btn grey darken-2" onClick={this.logOut} >Logout</button>
                     </div>
                 </div>
                 {listComponents}
                 <NewIngredient onUpdate={this.handleUpdate} />
             </div>
-        );       
+        );
     }
 }
 
